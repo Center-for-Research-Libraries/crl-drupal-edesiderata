88c66f3a545e14903073ca5bd9f77cc5adef06a3
Ryan Jacobs <rjacobs@422459.no-reply.drupal.org>
2016-09-14 17:22:01 -0500

    Maintenance update to salesforce module.

--- sites/all/modules/contrib/salesforce/salesforce.api.php
+++ sites/all/modules/contrib/salesforce/salesforce.api.php
@@ -38,13 +38,13 @@ function hook_salesforce_mapping_fieldmap_type_alter($fieldmap_type) {
  *   if no exisitng mapping object exists.
  * @param array $sf_object
  *   The salesforce object data that will be used in this pull.
- * @param object $mapping
+ * @param SalesforceMapping $mapping
  *   The salesforce mapping that will be used during the pull.
  */
 function hook_salesforce_pull_mapping_object_alter(&$mapping_object, $sf_object, $mapping) {
   // Do some prematching for incoming user pulls if we don't already have a
   // match.
-  if (!$mapping_object && $sf_mapping->drupal_entity_type == 'user') {
+  if (!$mapping_object && $mapping->drupal_entity_type == 'user') {
     // Run some complex custom prematching logic that searches for an existing
     // Drupal user that is a match to the incoming Salesforce data.
     $matched_user = mymodule_custom_sf_pull_prematch($sf_object, $mapping);
@@ -54,8 +54,8 @@ function hook_salesforce_pull_mapping_object_alter(&$mapping_object, $sf_object,
       // user instead of creating a new one.
       entity_create('salesforce_mapping_object', array(
         'salesforce_id' => $sf_object['Id'],
-        'entity_type' => $sf_mapping->drupal_entity_type,
-        'entity_id' => $match->uid,
+        'entity_type' => $mapping->drupal_entity_type,
+        'entity_id' => $matched_user->uid,
         'last_sync_message' => t('User prematch on pull'),
         'last_sync_status' => SALESFORCE_MAPPING_STATUS_SUCCESS
       ))->save();
@@ -72,13 +72,13 @@ function hook_salesforce_pull_mapping_object_alter(&$mapping_object, $sf_object,
  *   if no exisitng mapping object exists.
  * @param object $entity
  *   The Drupal entity that is being pushed.
- * @param object $mapping
+ * @param SalesforceMapping $mapping
  *   The salesforce mapping that will be used during the push.
  */
 function hook_salesforce_push_mapping_object_alter(&$mapping_object, $entity, $mapping) {
   // Do some prematching for outgoing user pushes if we don't already have a
   // match.
-  if (!$mapping_object && $sf_mapping->drupal_entity_type == 'user') {
+  if (!$mapping_object && $mapping->drupal_entity_type == 'user') {
     // Run some complex custom prematching logic that queries Salesforce for an
     // existing contact (beyond a basic external key comparison) that is a match
     // to this Drupal user.
@@ -88,9 +88,9 @@ function hook_salesforce_push_mapping_object_alter(&$mapping_object, $entity, $m
       // This means that the rest of the push logic will target this matched
       // contact instead of creating a new one.
       entity_create('salesforce_mapping_object', array(
-        'salesforce_id' => $sf_object['Id'],
-        'entity_type' => $sf_mapping->drupal_entity_type,
-        'entity_id' => $match->uid,
+        'salesforce_id' => $matched_contact['Id'],
+        'entity_type' => $mapping->drupal_entity_type,
+        'entity_id' => $entity->uid,
         'last_sync_message' => t('User prematch on push'),
         'last_sync_status' => SALESFORCE_MAPPING_STATUS_SUCCESS
       ))->save();
@@ -148,7 +148,10 @@ function hook_salesforce_mapping_entity_uris_alter(&$entity_uris) {
  *   FALSE if the entity should not be synced to Salesforce for this mapping/sync trigger.
  */
 function hook_salesforce_push_entity_allowed($entity_type, $entity, $sf_sync_trigger, $mapping) {
-
+  // Example: Don't sync the admin user.
+  if ($entity_type == 'user' && $entity->uid === 1) {
+    return FALSE;
+  }
 }
 
 /**
@@ -199,14 +202,47 @@ function hook_salesforce_query_alter(SalesforceSelectQuery &$query) {
  * @param object $result
  *   The salesforce response
  * @param array $synced_entity
- *   Entity data for this push. This array has 3 keys
+ *   Entity data for this push. This array has 4 keys
  *     'entity_wrapper': entity_metadata_wrapper() for the Drupal entity 
  *     'mapping_object': salesforce mapping object record, if it exists. 
  *       Otherwise null
- *     'queue_item': Drupal queue item corresponding to this push attempt
+ *     'queue_item': If this is a SOAP push, Drupal queue item corresponding to
+ *       this push attempt. Otherwise FALSE.
+ *     'mapping': SalesforceMapping being used for this push
  */
 function hook_salesforce_push_success($op, $result, $synced_entity) {
-  
+  $mapping_object = FALSE;
+  if (!empty($synced_entity['mapping_object'])) {
+    $mapping_object = $synced_entity['mapping_object'];
+  }
+  if (drupal_strtolower($op) == 'delete' && $mapping_object) {
+    $mapping_object->delete();
+    return;
+  }
+
+  if (!$mapping_object) {
+    // Create mapping object, saved below.
+    $wrapper = $synced_entities[$key]['entity_wrapper'];
+    list($entity_id) = entity_extract_ids($wrapper->type(), $wrapper->value());
+    $mapping_object = entity_create('salesforce_mapping_object', array(
+      'entity_id' => $entity_id,
+      'entity_type' => $wrapper->type(),
+      'salesforce_id' => $result->id,
+      'last_sync_message' => t('Mapping object created via !function.', array('!function' => __FUNCTION__)),
+    ));
+  }
+  else {
+    $mapping_object->last_sync_message = t('Mapping object updated via !function.', array('!function' => __FUNCTION__));
+  }
+
+  $mapping_object->last_sync_status = SALESFORCE_MAPPING_STATUS_SUCCESS;
+  $mapping_object->last_sync = REQUEST_TIME;
+  $mapping_object->last_sync_action = 'push';
+  $mapping_object->save();
+
+  watchdog('salesforce_push', '%op: Salesforce object %id',
+    array('%id' => $result->id, '%op' => $op)
+  );
 }
 
 /**
@@ -218,14 +254,39 @@ function hook_salesforce_push_success($op, $result, $synced_entity) {
  * @param object $result
  *   The salesforce response
  * @param array $synced_entity
- *   Entity data for this push. This array has 3 keys
+ *   Entity data for this push. This array has 4 keys
  *     'entity_wrapper': entity_metadata_wrapper() for the Drupal entity 
  *     'mapping_object': salesforce mapping object record, if it exists. 
  *       Otherwise null
- *     'queue_item': Drupal queue item corresponding to this push attempt
+ *     'queue_item': If this is a SOAP push, Drupal queue item corresponding to
+ *       this push attempt. Otherwise FALSE.
+ *     'mapping': SalesforceMapping being used for this push
  */
 function hook_salesforce_push_fail($op, $result, $synced_entity) {
-  
+  $error_messages = array();
+  foreach ($result->errors as $error) {
+    watchdog('salesforce_push', '%op error for Salesforce object %id. @code: @message',
+      array(
+        '%id' => $result->id,
+        '@code' => $error->statusCode,
+        '@message' => $error->message,
+        '%op' => $op,
+      ),
+      WATCHDOG_ERROR
+    );
+    $error_messages[] = $error->message;
+  }
+  if (!empty($synced_entity['mapping_object'])) {
+    $mapping_object = $synced_entity['mapping_object'];
+    $mapping_object->last_sync = REQUEST_TIME;
+    $mapping_object->last_sync_action = 'push';
+    $mapping_object->last_sync_status = SALESFORCE_MAPPING_STATUS_ERROR;
+    $mapping_object->last_sync_message = t('Push error via %function with the following messages: @message.', array(
+      '%function' => __FUNCTION__,
+      '@message' => implode(' | ', $error_messages),
+    ));
+    $mapping_object->save();
+  }
 }
 
 /**
@@ -236,7 +297,7 @@ function hook_salesforce_push_fail($op, $result, $synced_entity) {
  *   The Drupal entity object.
  * @param array $sf_object
  *   The Salesforce query result array.
- * @param SalesforceMapping $sf_object
+ * @param SalesforceMapping $sf_mapping
  *   The Salesforce Mapping being used to pull this record
  *
  * @throws SalesforcePullException
@@ -258,7 +319,7 @@ function hook_salesforce_pull_entity_presave($entity, $sf_object, $sf_mapping) {
  *   The Drupal entity object.
  * @param array $sf_object
  *   The SObject from the pull query (as an array).
- * @param SalesforceMapping $sf_object
+ * @param SalesforceMapping $sf_mapping
  *   The Salesforce Mapping being used to pull this record
  *
  * @throws SalesforcePullException
@@ -289,7 +350,7 @@ function hook_salesforce_pull_entity_insert($entity, $sf_object, $sf_mapping) {
  *   The Drupal entity object.
  * @param array $sf_object
  *   The SObject from the pull query (as an array).
- * @param SalesforceMapping $sf_object
+ * @param SalesforceMapping $sf_mapping
  *   The Salesforce Mapping being used to pull this record
  *
  * @throws SalesforcePullException
